86782816f358905ee807cd49d2af27999aea7683
programandoando <programandoandohamp@gmail.com>
2014-08-06 22:14:02 -0500

    cambios

--- modelos/principal.js
+++ modelos/principal.js
@@ -1,163 +1,20 @@
-//CAMBIO! ahora
-var Sequelize = require("sequelize");
+var Sequelize=require("sequelize");
 
-//----- CONFIGURAR LA BASE DE DATOS con sequelize
-var sequelize = new Sequelize("NOMBRE_BASE","USUARIO","PASSWORD_USUARIO",{
-	dialect:"sqlite", //OTROS VALORES: postgres, mysql, mariadb
-	//la propiedad storage SOLO ES PARA sqlite
-	storage:__dirname + "/database.db",
+var sequelize=new Sequelize("NOMBRE_BASE","USUARIO","PASSWORD",{
+	dialect:"sqlite",
+	storage:__dirname+"/database.db",
 	define:{
 		timestamps:false,
-		//deshabilita la convencion por default para el nombre de las tablas
 		freezeTableName:true
 	}
 });
 
-//SINCRONA
-   // leer archivo
-   // EJECUTAMOS LA SIGUIENTE LINEA
-
-//---- LAS OPERACIONES EN DISCO DURO EN NODE SE HACEN DE MANER ASINCRONA
-// CALLBACKS
-// success ES EL CALLBACK DE LA OPERACION ASINCRONA
 sequelize.authenticate().success(function(){
-	//EL CODIGO EN ESTA FUNCION SE EJECTUA
-	//SOLO HASTA QUE LA OPERCION ASINCRONA (AUTHENTICATE) TERMINA
+	console.log("base lista");
 	
-	console.log("base lista!");
 });
 
-//module.exports ES UN OBJETO QUE NOS PERMITE HACER VISIBLES
-//DATOS DE ESTE ARCHIVO
-module.exports.PRUEBA = "hola";
 
-
-
-// ------------ MAPEOS DE LAS TABLAS CON SEQUELIZE ------
-
-var Articulo = sequelize.define("Articulo",{
-	id:{
-		//LE INDICAMOS A SEQUELIZE CON primaryKey que esta es una col
-		//de la la llave primaria
-		primaryKey:true,
-		type:Sequelize.INTEGER
-	},
-	//le dice a sequelize que tenemos una columan titulo
-	//que es una cadena
-	titulo:Sequelize.TEXT,
-	contenido:Sequelize.TEXT,
-	//Sequelize.DATE ES PARA FECHAS
-	fecha_creacion:Sequelize.DATE
-},{
-	//tableName le dicen cual es la tabla de la base
-	//que esta asociada a este objeto
-	tableName:"articulos"
-});
-
-//-------- SOLUCION EJERCICIO MAPEOS ------------
-var Usuario = sequelize.define("Usuario",{
-	id:{
-		primaryKey:true,
-		type:Sequelize.INTEGER
-	},
-	nombre:Sequelize.TEXT,
-	email:Sequelize.TEXT,
-	password:Sequelize.TEXT
-},{
-	tableName:"usuarios"
-});
-
-//-------- SOLUCION EJERCICIO FIND ALL-----
-var Categoria = sequelize.define("Categoria",{
-	id:{
-		primaryKey:true,
-		type:Sequelize.INTEGER
-	},
-	nombre:Sequelize.TEXT
-},{
-	tableName:"categorias"
-}); 
-
-//--------- SOLUCION EJERCICIO MAPEOS
-
-var Comentario = sequelize.define("Comentario",{
-	id:{
-		primaryKey:true,
-		type:Sequelize.INTEGER
-	},
-	comentario:Sequelize.TEXT
-},{
-	tableName:"comentarios"
-});
-
-//SOLUCION MAPEO 1-N
-Articulo.hasMany(Comentario,{
-	foreignKey:"articulo_id",
-	//as me permite obtener los comentarios del articulo
-	//haciendo articulo.comentarios
-	as:"comentarios"
-});
-
-// -------- EJEMPLO MAPEO 1-N ------------------
-// un usuario tiene muchos articulos
-Usuario.hasMany(Articulo,{
-	//forei	gnKey es la col que sirve de pegamento en la relacion 1-N
-	foreignKey:"usuario_id",
-	//cuando obtengan un objeto usuario
-	// pueden acceder a los articulos con usuario.articulos
-	as:"articulos"
-});
-
-// articulo le pertenece a un Usuario
-// TOMAMOS EL SENTIDO INVERSO DE LA RELACION 1-N
-Articulo.belongsTo(Usuario,{
-	foreignKey:"usuario_id",
-	//el parametro as me permite que al articulo puede acceder
-	//al usuario que tiene este articulo con articulo.usuario
-	as:"usuario"	
-});
-
-//-------------------- EJEMPLO MAPEO N-N -------------------
-Articulo.hasMany(Categoria,{
-	foreignKey:"articulo_id",
-	as:"categorias",
-	//este parametro es solo para la relacion N-N
-	through:"categorias_articulos"
-});
-
-Categoria.hasMany(Articulo,{
-	foreignKey:"categoria_id",
-	as:"articulos",
-	through:"categorias_articulos"
-});
-
-var DatosUsuario = sequelize.define("DatosUsuario",{
-	id:{
-		primaryKey:true,
-		type:Sequelize.INTEGER
-	},
-	biografia:Sequelize.TEXT,
-	fecha_registro:Sequelize.DATE
-},{
-	tableName:"datos_usuarios"
-});
-
-//--------- EJEMPLO MAPEO 1-1
-Usuario.hasOne(DatosUsuario,{
-	foreignKey:"usuario_id",
-	as:"datosUsuario"
-});
-
-DatosUsuario.hasOne(Usuario,{
-	foreignKey:"usuario_id",
-	as:"usuario"
-});
-
-//EXPORTANDO EL MODELO DE LA TABLA ARTICULO
-module.exports.Articulo = Articulo;
-module.exports.Usuario = Usuario;
-module.exports.Categoria = Categoria;
-module.exports.Comentario = Comentario;
-module.exports.DatosUsuario = DatosUsuario;
+module.exports.PRUEBA="hola";
 
 
--- servidor.js
+++ servidor.js
@@ -1,13 +1,38 @@
 var express =require("express");
+var nunjucks =require("nunjucks");
+var modelos=require("./modelos/principal.js");
+
+console.log("PRUEBA:"+modelos.PRUEBA);
+
+
 var app=express();
+nunjucks.configure(__dirname+"/vistas",{
+	express:app
+});
+
+
 
 app.listen(8084);
-app.get("/inicio",function(req,res){
-	res.send("Hola!");
+app.get("/articulo",function(req,res){
+	//res.send("Holadd!");
+	res.render("articulo.html");
 	
 });
 
-app.get("/informes",function(req,res){
+app.get("/blog",function(req,res){
+	//res.send("Hola!");
+	res.render("blog.html");
+	
+});
+
+app.get("/usuario",function(req,res){
+	//res.send("Hola!");
+	res.render("usuario.html");
+	
+});
+
+
+app.get("/informe",function(req,res){
 	res.send("Informes aqui!");
 	
 });
